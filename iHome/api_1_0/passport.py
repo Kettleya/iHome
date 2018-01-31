ee68ef1dacbaf21eda613c80d048a637cac90f6b
Kettleya <kekechuia@gmail.com>
2018-01-31 17:26:45 +0800

    实现用户登陆功能

--- iHome/api_1_0/passport.py
+++ iHome/api_1_0/passport.py
@@ -1,12 +1,62 @@
 # -*- coding:utf-8 -*-
-
+import re
 from flask import current_app, jsonify
 from flask import request
-
+from flask import session
 from iHome import redis_store, db
 from iHome.models import User
 from iHome.utils.response_code import RET
 from . import api
+
+
+@api.route('/session', methods=['POST'])
+def login():
+    """
+    1.获取参数并校验
+    2.判断手机号是否符合规范
+    3.从数据库中获取用户信息
+    4.校验密码是否相等
+    5.把当前用户信息存入到session中
+    6.返回并给出响应
+    :return:
+    """
+
+    # 1.获取参数并校验,2.判断手机号是否符合规范
+    data_dict = request.json
+    mobile = data_dict.get('mobile')
+    password = data_dict.get('password')
+
+    if not all([mobile,password]):
+        return jsonify(errno=RET.PARAMERR, errmsg='参数有误')
+
+    if not re.match('^1[34578][0-9]{9}$', mobile):
+        return jsonify(errno=RET.PARAMERR, errmsg='手机号格式有误')
+
+    # 3.从数据库中获取用户信息
+    try:
+        user = User.query.filter(User.mobile == mobile).first()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg='数据查询失败')
+
+    if not user:
+        return jsonify(errno=RET.USERERR, errmsg='用户不存在')
+
+    # 4.校验密码是否相等
+    if not user.check_password(password):
+        return jsonify(errno=RET.PWDERR, errmsg='密码错误')
+
+    # 5.把当前用户信息存入到session中
+    try:
+        session['user_id']=user.id
+        session['name'] = user.name
+        session['mobile'] = user.mobile
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg='用户登陆失败')
+
+    # 6.返回并给出响应
+    return jsonify(errno=RET.OK, errmsg='登陆成功')
 
 
 @api.route('/users', methods=['POST'])
@@ -34,14 +84,14 @@ def register():
         real_phonecode = redis_store.get(mobile)
     except Exception as e:
         current_app.logger.error(e)
-        return jsonify(errno=RET.DBERR,errmsg='查询短信验证码失败')
+        return jsonify(errno=RET.DBERR, errmsg='查询短信验证码失败')
 
     if not real_phonecode:
-        return jsonify(errno=RET.NODATA,errmsg='短信验证码已经过期')
+        return jsonify(errno=RET.NODATA, errmsg='短信验证码已经过期')
 
     # 3.进行短信验证码的对比
     if real_phonecode != phonecode:
-        return jsonify(errno=RET.DATAERR,errmsg='短信验证码输入错误')
+        return jsonify(errno=RET.DATAERR, errmsg='短信验证码输入错误')
 
     # 4.初始化User,保存相关数据
     user = User()
@@ -57,7 +107,7 @@ def register():
     except Exception as e:
         db.session.rollback()
         current_app.logger.error(e)
-        return jsonify(errno=RET.DBERR,errmsg='保存用户数据失败')
+        return jsonify(errno=RET.DBERR, errmsg='保存用户数据失败')
 
     # 6.给出响应
-    return jsonify(errno=RET.OK,errmsg='注册成功')
+    return jsonify(errno=RET.OK, errmsg='注册成功')
--- iHome/static/js/ihome/login.js
+++ iHome/static/js/ihome/login.js
@@ -10,20 +10,43 @@ $(document).ready(function() {
     $("#password").focus(function(){
         $("#password-err").hide();
     });
-    // TODO: 添加登录表单提交操作
+    // TODO: 添加登录表单提交操
     $(".form-login").submit(function(e){
         e.preventDefault();
+        //取值
         mobile = $("#mobile").val();
-        passwd = $("#password").val();
+        password = $("#password").val();
         if (!mobile) {
             $("#mobile-err span").html("请填写正确的手机号！");
             $("#mobile-err").show();
             return;
         } 
-        if (!passwd) {
+        if (!password) {
             $("#password-err span").html("请填写密码!");
             $("#password-err").show();
             return;
         }
+        var params ={
+            'mobile':mobile,
+            'password':password
+        }
+
+        $.ajax({
+            url:'/api/v1.0/session',
+            type:'post',
+            data:JSON.stringify(params),
+            contentType:'application/json',
+            headers:{
+                'X-CSRFToken':getCookie('csrf_token'),
+            },
+            success:function (resp) {
+                if (resp.errno == '0'){
+                    location.href='/'
+                }else{
+                    $('#password-err span').html(resp.errmsg)
+                    $('#password-err').show()
+                }
+            }
+        })
     });
 })
